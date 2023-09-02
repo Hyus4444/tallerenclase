80f3b92c5cf9d3040a6c5cc4768d3a874e610cd0
xd <123@o.com>
2023-09-02 11:47:21 -0500

    trabajo en js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-$("#container1").hide();
+$("#container1").on("click",function(){
+    $("#container2").show();
+    $("#container1").hide();
+    $("btn-start").hide();
+
+});
+
+$("#container1").on("mouseover",function(){
+    $("#container1").addClass("forma");
+
+});
+
+$("#container2").on("mouseover",function(){
+    $("#container2>h1").text("Hola uwu")
+
+$("#container2>h1").append("<i>Hola uwu</i>")
+$("#container2>h1").append("<marquee>Hola uwu<marquee>")
+});
